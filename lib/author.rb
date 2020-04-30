e39a7df99c4fd6ccef0c4e8abea9cf6879c1c457
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-30 05:12:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,26 +16,28 @@
 class Author
   attr_accessor :name, :posts   # #name - has an attr_accessor for name
 
-  def initialize(name)  # #new - is initialized with a name
+  def initialize(name)          # #new - is initialized with a name
     @name = name
   end
 
-  def posts             # #posts - has many posts
+  def posts                     # #posts - has many posts
     Post.all
   end
 
-  def add_post(post)     #takes in an argument of a post and associates that post with the
-    post.author = self    # author by telling the post that it belongs to that author
+  def add_post(post)            #takes in an argument of a post and associates that post with the
+    post.author = self          # author by telling the post that it belongs to that author
   end
 
-  def add_post_by_title(post_title)  # takes in an argument of a post title, creates a new post with it and associates the post and author
-    author = Post.new(post_title)
+  def add_post_by_title(post_title)  # takes in an argument of a post title, 
+    author = Post.new(post_title)    # creates a new post with it and associates the post and author
     add_post(author)
   end
 
   def self.post_count    # is a class method that returns the total number of posts associated to all existing authors
     Post.all.count
   end
+
+  
 
 
 
--- lib/post.rb
+++ lib/post.rb
@@ -1,39 +1,34 @@
 # Post
 #   #new
-#     is initialized with an argument of a title (FAILED - 11)
-#     pushes new instances into a class variable called @@all upon initialization (FAILED - 12)
+#     is initialized with an argument of a title 
+#     pushes new instances into a class variable called @@all upon initialization
 #   @@all
-#     is a class variable set to an array (FAILED - 13)
+#     is a class variable set to an array
 #   .all
-#     is a class method that returns an array of all post instances that have been created (FAILED - 14)
+#     is a class method that returns an array of all post instances that have been created
 #   #title
-#     has a title (FAILED - 15)
+#     has a title
 #   #author
-#     belongs to an author (FAILED - 16)
+#     belongs to an author
 #   #author_name
-#     knows the name of its author (FAILED - 17)
-#     returns nil if the post does not have an author (FAILED - 18)
+#     knows the name of its author
+#     returns nil if the post does not have an author
 
 class Post
-  attr_accessor :title, :author  #  has a title, belongs to an author,
+  attr_accessor :title, :author         #  has a title, belongs to an author,
 
-  @@all = []              # @@all - is a class variable set to an array
+  @@all = []                            # @@all - is a class variable set to an array
 
-  def initialize(title)   # #new - is initialized with an argument of a title
-    @title = title        # pushes new instances into a class variable called @@all upon initialization
+  def initialize(title)                 # #new - is initialized with an argument of a title
+    @title = title                      # pushes new instances into a class variable called @@all upon initialization
     @@all << self
   end
 
-  def self.all      #.all - is a class method that returns an array of all post instances that have been created
+  def self.all                          #.all - is a class method that returns an array of all post instances that have been created
     @@all
   end
 
   def author_name
     author.name unless author.nil?      #knows the name of its author
   end                                   # returns nil if the post does not have an author
-
-
-
-
-
 end
